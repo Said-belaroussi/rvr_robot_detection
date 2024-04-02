0ea28c0555029250ddacaee9193ef7e0fc070ac0
Said-belaroussi <said.belaroussi@ulb.be>
2024-04-02 15:06:42 +0200

    updated simple bounding box detector

--- oak_detector/oak_bb.py
+++ oak_detector/oak_bb.py
@@ -1,14 +1,22 @@
+import os.path
+import json
+import sys
+import cv2
+import depthai as dai
+import numpy as np
+import time
+
 import rospy
 from std_msgs.msg import Header
 from vision_msgs.msg import Detection2DArray
 from vision_msgs.msg import Detection2D
 from vision_msgs.msg import BoundingBox2D
+from geometry_msgs.msg import Pose2D
+
 import rospkg
 
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 
-def publisher_bbox(bbox_data):
+def publisher_bbox(i):
     """
     Function to publish the bounding box information as a Detection2DArray message
     """
@@ -21,59 +29,114 @@ def publisher_bbox(bbox_data):
     detection_2d_msg = Detection2D()
     detection_2d_msg.header = header
     bounding_box_2d = BoundingBox2D()
-    bounding_box_2d.center.x = bbox_data["x"]
-    bounding_box_2d.center.y = bbox_data["y"]
-    bounding_box_2d.size_x = bbox_data["width"]
-    bounding_box_2d.size_y = bbox_data["height"]
+    pose_2d = Pose2D()
+    pose_2d.x = i["x"]
+    pose_2d.y = i["y"]
+    pose_2d.theta = i["confidence"]
+    bounding_box_2d.center = pose_2d
+    bounding_box_2d.size_x = i["width"]
+    bounding_box_2d.size_y = i["height"]
     detection_2d_msg.bbox = bounding_box_2d
     detection_2d_list.append(detection_2d_msg)
 
     msg.detections = detection_2d_list
     pub_bbox.publish(msg)
 
-def start_oak_camera(blob_filename, json_filename):
+def get_config_from_json(json_filename):
+    """
+    Function to read configuration parameters from a JSON file
+    """
+    f = open(json_filename)
+    jsonData = json.load(f)
+    f.close()
+    confidenceThreshold = jsonData["nn_config"]["NN_specific_metadata"]["confidence_threshold"]
+    numClasses = jsonData["nn_config"]["NN_specific_metadata"]["classes"]
+    anchors = jsonData["nn_config"]["NN_specific_metadata"]["anchors"]
+    anchorMasks = jsonData["nn_config"]["NN_specific_metadata"]["anchor_masks"]
+    coordinateSize = jsonData["nn_config"]["NN_specific_metadata"]["coordinates"]
+    iouThreshold = jsonData["nn_config"]["NN_specific_metadata"]["iou_threshold"]
+    inputSize = jsonData["nn_config"]["input_size"]
+    inputSizeX, inputSizeY = inputSize.split("x")
+    inputSizeX = int(inputSizeX)
+    inputSizeY = int(inputSizeY)
+    labelMap = jsonData["mappings"]["labels"]
 
-    # Set up ROS publisher for bounding boxes
-    rospy.init_node("oak_detector", anonymous=True)
-    pub_bbox = rospy.Publisher("oak", Detection2DArray, queue_size=1)
+    return confidenceThreshold, numClasses, anchors, anchorMasks, \
+        coordinateSize, iouThreshold, inputSizeX, inputSizeY, labelMap
 
-    # Load configuration parameters from JSON file
-    confidenceThreshold, numClasses, anchors, anchorMasks, \
-        coordinateSize, iouThreshold, inputSizeX, inputSizeY, labelMap = get_config_from_json(json_filename)
+def start_oak_camera(blob_filename, json_filename, compressed=True):
+
+    (confidenceThreshold, numClasses, anchors, anchorMasks,
+     coordinateSize, iouThreshold, inputSizeX, inputSizeY, labelMap) = get_config_from_json(json_filename)
 
     # Define camera pipeline and its components
     pipeline = dai.Pipeline()
 
-    # Define needed components (replace YOLO spatial detection network with a standard object detection network)
+    # Define needed components
     camRgb = pipeline.create(dai.node.ColorCamera)
-    object_detection_network = pipeline.create(dai.node.MobileNetDetectionNetwork)
-    object_detection_network.setConfidenceThreshold(confidenceThreshold)
-    object_detection_network.setBoundingBoxScaleFactor(0.5)
-    object_detection_network.setBlobPath(blob_filename)
-    object_detection_network.setNumClasses(numClasses)
+    yolo_detection_network = pipeline.create(dai.node.YoloDetectionNetwork)
 
-    camRgb.preview.link(object_detection_network.input)
+    xoutRgb = pipeline.create(dai.node.XLinkOut)
+    xoutNN = pipeline.create(dai.node.XLinkOut)
 
-    xout = pipeline.create(dai.node.XLinkOut)
-    xout.setStreamName("detections")
-    object_detection_network.out.link(xout.input)
+    xoutRgb.setStreamName("rgb")
+    xoutNN.setStreamName("detections")
+
+    # RGB camera setup
+    camRgb.setPreviewSize(inputSizeX, inputSizeY)
+    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+    # If NN take horizontally compressed images as input. Increases FOV
+    if compressed:
+        camRgb.setPreviewKeepAspectRatio(False)
+
+    yolo_detection_network.setBlobPath(blob_filename)
+    yolo_detection_network.setConfidenceThreshold(confidenceThreshold)
+    yolo_detection_network.input.setBlocking(False)
+
+    yolo_detection_network.setNumClasses(numClasses)
+    yolo_detection_network.setCoordinateSize(coordinateSize)
+    yolo_detection_network.setAnchors(anchors)
+    yolo_detection_network.setAnchorMasks(anchorMasks)
+    yolo_detection_network.setIouThreshold(iouThreshold)
+
+    camRgb.preview.link(yolo_detection_network.input)
+    yolo_detection_network.passthrough.link(xoutRgb.input)
+    yolo_detection_network.out.link(xoutNN.input)
 
     # Connect to device and start processing
     with dai.Device(pipeline) as device:
-        outputQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+        previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
 
         while True:
-            detections = outputQueue.get().detections
+            inPreview = previewQueue.get()
+            inDet = detectionNNQueue.get()
+
+            frame = inPreview.getCvFrame()
+            detections = inDet.detections
+
+            height, width = frame.shape[0], frame.shape[1]
 
             for detection in detections:
-                bbox_data = {
-                    "x": detection.xmin,
-                    "y": detection.ymin,
-                    "width": detection.xmax - detection.xmin,
-                    "height": detection.ymax - detection.ymin
-                }
+                bbox_data = dict()
+
+                x1 = int(detection.xmin * width)
+                x2 = int(detection.xmax * width)
+                y1 = int(detection.ymin * height)
+                y2 = int(detection.ymax * height)
+
+                # Bounding box info for detection ros msg
+                bbox_data["x"] = (x1 + x2) / 2
+                bbox_data["y"] = (y1 + y2) / 2
+                bbox_data["width"] = x2 - x1
+                bbox_data["height"] = y2 - y1
+                bbox_data["confidence"] = detection.confidence
 
                 publisher_bbox(bbox_data)  # Publish the bounding box data
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -82,5 +145,9 @@ if __name__ == "__main__":
     else:
         blob_filename = sys.argv[1]
         json_filename = sys.argv[2]
+
+    # Set up ROS publisher for bounding boxes
+    rospy.init_node("oak_detector", anonymous=True)
+    pub_bbox = rospy.Publisher("oak", Detection2DArray, queue_size=1)
 
     start_oak_camera(blob_filename, json_filename)
